5823f352917bce09d159f8dbba0faafd44181154
Shilam Sai Bhargavi <178761237+ssaibhargavi@users.noreply.github.com>
2025-07-16 09:06:32 +0530

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,5 @@
+# Converting the temperature from Fahrenheit (°F) to Celsius (°C).
+
+Fahrenheit = int(input("Enter temperature in fahrenheit: "))
+Celsius = (Fahrenheit - 32)/1.8
+print("Temperature in celsius: ",Celsius)
