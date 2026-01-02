d0850a13d04018837b6eb6b14b35c14096fafe96
Shilam Sai Bhargavi <178761237+ssaibhargavi@users.noreply.github.com>
2026-01-02 14:28:49 +0530

    Fix comment for FizzBuzz logic in fizzbuzz.py
    Corrected the comment to clarify the conditions for printing Fizz, Buzz, and FizzBuzz.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-# the code prints FizzBuzz if it the multiple of 3 and 5, prints Fizz if the number is only the multiple of 3, it prints buzz if the number is the multiple of 5 and it prints the given number if the above conditions failec
+# the code prints FizzBuzz if the number is the multiple of 3 and 5, prints Fizz if the number is only the multiple of 3, it prints buzz if the number is the multiple of 5 and it prints the given number if the above conditions failec
 a=int(input())
 if a%3==0 and a%5==0:
   print("FizzBuzz")
