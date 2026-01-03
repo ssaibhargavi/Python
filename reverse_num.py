3315d59051d2916e812104cba412dedc6aa2abba
Shilam Sai Bhargavi <178761237+ssaibhargavi@users.noreply.github.com>
2026-01-03 21:27:38 +0530

    Create reverse_num.py

---
+++ reverse_num.py
@@ -0,0 +1,9 @@
+# Reverse of a number 
+
+number = int(input("Enter a number: "))
+reverse = 0
+while number != 0:
+    digit = number % 10
+    reverse = reverse * 10 + digit
+    number //= 10   
+print(reverse)  
