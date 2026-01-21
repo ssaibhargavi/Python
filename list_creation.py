cc1b25d5c7ff9ade6c2c8051157c5761151c8aff
Shilam Sai Bhargavi <178761237+ssaibhargavi@users.noreply.github.com>
2026-01-21 12:26:22 +0530

    Add list creation functionality in list_creation.py

---
+++ list_creation.py
@@ -0,0 +1,8 @@
+# creating a list
+
+list1=[]
+elements=int(input("No.of elements in the list:"))
+for i in range(elements):
+    element=int(input("Enter element:"))
+    list1.append(element)
+print(list1)
